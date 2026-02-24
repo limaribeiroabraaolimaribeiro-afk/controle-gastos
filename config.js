5285bad1cf4916d501084b2150189748640a4fde
limaribeiroabraaolimaribeiro-afk <limaribeiroabraaolimaribeiro@gmail.com>
2026-02-24 11:11:39 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // 1) não coloque barra "/" no final da URL
 // 2) não coloque ponto e vírgula dentro das aspas
 
-window.API_BASE_URL = "https://proportion-shaped-his-approach.trycloudflare.com";
+window.API_BASE_URL = "http://192.168.20.13:3000";
