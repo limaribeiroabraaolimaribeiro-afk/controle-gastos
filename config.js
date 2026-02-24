4d0e7f689bdc65f5ea240c2178570938116a571d
limaribeiroabraaolimaribeiro-afk <limaribeiroabraaolimaribeiro@gmail.com>
2026-02-24 01:00:25 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 // config.js
-// ⚠️ Importante: o ; (ponto e vírgula) fica FORA das aspas.
+// ⚠️ IMPORTANTE:
+// 1) não coloque barra "/" no final da URL
+// 2) não coloque ponto e vírgula dentro das aspas
 
-window.API_BASE_URL = "https://compliant-jaguar-instructional-consumption.trycloudflare.com";
+window.API_BASE_URL = "https://proportion-shaped-his-approach.trycloudflare.com";
